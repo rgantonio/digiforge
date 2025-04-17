46940bf5fb37f142c7511297ae4fcd8ca07b84d0
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-04-17 15:40:37 +0200

    tb: update uart to use tasks

--- tb/uart/tb_uart_txrx.sv
+++ tb/uart/tb_uart_txrx.sv
@@ -66,22 +66,12 @@ module tb_uart_tx;
     forever #50 clk_i = ~clk_i; // Toggle clock every 50 time units
   end
 
-  // To get a baudrate of 9600 given a clock of 10 MHz
-  // Baud rate = Clock frequency / (16 * Baud rate)
-  // Baud rate = 10 MHz / (16 * 9600) = 65.1
-  // So we need to set the baud rate to 65.1
-  // Round up to the nearest integer is 66
-  initial begin
-
-    // Initialize inputs
-    rst_ni        = 0;
-    baud_rate_i   = 66;
-    data_i        = $urandom & 8'hFF;
-    start_i       = 0;
-
-    // Assert reset
+  // Task for loading the UART and waiting for result
+  task load_and_wait();
+    // Initialize data
+    data_i  = $urandom & 8'hFF;
+    start_i = 0;
     clk_delay(3);
-    #1; rst_ni = 1;
 
     // Start transmission
     clk_delay(3);
@@ -100,22 +90,30 @@ module tb_uart_tx;
     $display("Data received successfully: %0h", rx_data_o);
 
     clk_delay(3);
+  endtask
 
-    // Do another transmission
-    data_i = $urandom & 8'hFF;
+
+  // To get a baudrate of 9600 given a clock of 10 MHz
+  // Baud rate = Clock frequency / (16 * Baud rate)
+  // Baud rate = 10 MHz / (16 * 9600) = 65.1
+  // So we need to set the baud rate to 65.1
+  // Round up to the nearest integer is 66
+  initial begin
+
+    // Initialize inputs
+    rst_ni        = 0;
+    baud_rate_i   = 66;
+    data_i        = 0;
+    start_i       = 0;
+
+    // Assert reset
     clk_delay(3);
+    #1; rst_ni = 1;
 
-    #1; start_i = 1;
-    clk_delay(1);
-    #1; start_i = 0;
-
-    // Wait for receiver to finish
-    wait(rx_done_o);
-
-    assert(rx_data_o == data_i) else begin
-      $error("Data mismatch: expected %0h, got %0h", data_i, rx_data_o);
+    for (int i = 0; i < 10; i++) begin
+      // Load and wait for the result
+      load_and_wait();
     end
-    $display("Data received successfully: %0h", rx_data_o);
 
     // Just trail the simulation
     clk_delay(25);
